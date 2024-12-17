061abd921a451294c59b183a3d600512c786047e
berti <berti@CLASSES.LOCAL>
2024-12-17 11:06:03 +0100

    nested layout

---
+++ app/reviews/layout.jsx
@@ -0,0 +1,11 @@
+export default function ReviewsLayout({children}) 
+{
+    return (
+        <div>
+            <div>
+                Barre de menu spécifique à review
+            </div>
+            {children}
+        </div>
+    )
+}
