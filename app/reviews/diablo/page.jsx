2566eb048b1a1b6db145c2df6fd4ee3d3e7a4ecf
berti <berti@CLASSES.LOCAL>
2024-12-17 09:54:48 +0100

    routing

---
+++ app/reviews/diablo/page.jsx
@@ -0,0 +1,8 @@
+export default function DiabloPage() {
+    return (
+        <>
+            <h1>Diablo 4</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga animi reiciendis magnam omnis ex molestiae error incidunt. Suscipit earum architecto, in eaque voluptatem ex, asperiores ratione porro hic odio ipsam quia rerum nihil ullam error alias. Adipisci quae at ad reprehenderit assumenda optio ut eveniet asperiores quidem dolorum! Expedita quidem officiis unde id eligendi vero ex explicabo, perspiciatis cupiditate aspernatur!</p>
+        </>
+    )
+}
