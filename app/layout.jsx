02afce64cea57d13e62f52aa8b0c4949d076f811
berti <berti@CLASSES.LOCAL>
2024-12-17 12:03:53 +0100

    tailwind utilisation

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,7 +5,7 @@ export default function RootLayout({children})
 {
     return (
         <html lang="fr">
-            <body>
+            <body className="flex flex-col px-4 py-2 min-h-screen bg-orange-50">
                 <header>
                     <nav>
                         <ul>
@@ -15,11 +15,11 @@ export default function RootLayout({children})
                         </ul>
                     </nav>
                 </header>
-                <main>
+                <main className="grow py-3">
                     {children}
                 </main>
-                <footer>
-                    footer
+                <footer className="text-center text-xs border-t py-3">
+                    Game Data and images courtesy of <a href="https://rawg.io/" target="_blank" className="text-orange-800 hover:underline">RAWG</a>
                 </footer>
             </body>
         </html>
