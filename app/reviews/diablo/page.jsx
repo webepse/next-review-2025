180433c1bdc025e1d167b211d256983599df4eef
Boderio <boderiotrigan@hotmail.com>
2024-12-18 11:54:16 +0100

    Headlessui et horiincs + components NavBars et Images

--- app/reviews/diablo/page.jsx
+++ app/reviews/diablo/page.jsx
@@ -3,6 +3,7 @@ export default function DiabloPage() {
     return (
         <>
             <Heading>Diablo 4</Heading>
+            <img src="/images/diablo.jpg" alt="Image de Diablo 4" className='mb-4 rounded w-screen mx-auto' />
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga animi reiciendis magnam omnis ex molestiae error incidunt. Suscipit earum architecto, in eaque voluptatem ex, asperiores ratione porro hic odio ipsam quia rerum nihil ullam error alias. Adipisci quae at ad reprehenderit assumenda optio ut eveniet asperiores quidem dolorum! Expedita quidem officiis unde id eligendi vero ex explicabo, perspiciatis cupiditate aspernatur!</p>
         </>
     )
--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -6,9 +6,19 @@ export default function ReviewsPage(){
         <>
             <Heading>Reviews</Heading>
             <p>Here we'll list all the reviews</p>
-            <ul>
-                <li><Link href="/reviews/diablo">Diablo IV</Link></li>
-                <li><Link href="/reviews/spider-man">SpiderMan</Link></li>
+            <ul className="flex flex-col gap-3">
+                <li className="border rounded shadow hover:shadow-xl w-80 bg-slate-50">
+                    <Link href="/reviews/diablo">
+                        <img src="/images/diablo.jpg" alt="Image de Diablo" className="rounded-t" />
+                        <h2 className="font-orbitron font-semibold py-1 text-center">Diablo IV</h2>
+                    </Link>
+                </li>
+                <li className="border rounded shadow hover:shadow-xl w-80 bg-slate-50">
+                    <Link href="/reviews/spider-man">
+                        <img src="/images/spiderman2.jpg" alt="Image de Diablo" className="rounded-t" />
+                        <h2 className="font-orbitron font-semibold py-1 text-center">Spider-man 2</h2>
+                    </Link>
+                </li>
             </ul>
         </>
     )
