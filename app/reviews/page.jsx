2d928ca2cb51ef959c6ce5f870feb2cf1339c3e7
berti <berti@CLASSES.LOCAL>
2025-01-07 11:28:47 +0100

    listing files et rendrering arrays

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,24 +1,23 @@
 import Link from "next/link";
 import Heading from '@/components/Heading'
+import { getReviews } from "@/lib/review";
 
-export default function ReviewsPage(){
+export default async function ReviewsPage(){
+    const reviews = await getReviews()
+    console.log('[ReviewsPage] reviews: ', reviews)
     return (
         <>
             <Heading>Reviews</Heading>
             <p>Here we'll list all the reviews</p>
-            <ul className="flex flex-col gap-3">
-                <li className="border rounded shadow hover:shadow-xl w-80 bg-slate-50">
-                    <Link href="/reviews/diablo">
-                        <img src="/images/diablo.jpg" alt="Image de Diablo" className="rounded-t" />
-                        <h2 className="font-orbitron font-semibold py-1 text-center">Diablo IV</h2>
-                    </Link>
-                </li>
-                <li className="border rounded shadow hover:shadow-xl w-80 bg-slate-50">
-                    <Link href="/reviews/spider-man">
-                        <img src="/images/spiderman2.jpg" alt="Image de Diablo" className="rounded-t" />
-                        <h2 className="font-orbitron font-semibold py-1 text-center">Spider-man 2</h2>
-                    </Link>
-                </li>
+            <ul className="flex flex-row flex-wrap gap-3">
+                {reviews.map((review)=>(
+                    <li key={review.slug} className="border rounded shadow hover:shadow-xl w-80 bg-slate-50">
+                        <Link href={`/reviews/${review.slug}`}>
+                            <img src={review.image} alt={`image de ${review.title}`} className="rounded-t" />
+                            <h2 className="font-orbitron font-semibold py-1 text-center">{review.title}</h2>
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </>
     )
---
+++ content/reviews/spiderman.md
@@ -0,0 +1,8 @@
+---
+title: "Spider Man"
+date: "2024-01-07"
+image: "/images/spiderman2.jpg"
+---
+
+## Appropriez-vous sanctuaire
+La bataille entre __les cieux__ et __les enfers__ fait rage et le chaos menace de consumer __Sanctuaire__. Avec d'innombrables démons à massacrer, une multitude de capacités à maitriser, des donjons _cauchemardesques_ et des butins légendaires, ce vaste monde ouvert vous promet aventures et dévastation. Survivez et Triomphez des ténèbres.. ou sombrez dans l'oubli le plus profond.
