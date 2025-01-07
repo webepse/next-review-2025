5d267d8d36c74539b0e968dcbb42d2b0a054a61d
berti <berti@CLASSES.LOCAL>
2025-01-07 10:03:56 +0100

    refactoring getReview / lib

--- app/reviews/diablo/page.jsx
+++ app/reviews/diablo/page.jsx
@@ -1,19 +1,15 @@
 import Heading from '@/components/Heading'
-import { readFile } from 'node:fs/promises'
-import { marked } from 'marked'
-import matter from "gray-matter"
+import { getReview } from '@/lib/review'
 
 export default async function DiabloPage() {
-    const text = await readFile('./content/reviews/diablo.md', 'utf-8')
-    const { content, data: {title,date,image}} = matter(text)
-    const html = marked(content)
+    const review = await getReview('diablo')
 
     return (
         <>
-            <Heading>{title}</Heading>
-            <p className='italic pb-2'>{date}</p>
-            <img src={image} alt={`Image de ${title}`} className='mb-4 rounded w-screen mx-auto' />
-            <article dangerouslySetInnerHTML={{__html: html}} className='prose w-full max-w-none'/>
+            <Heading>{review.title}</Heading>
+            <p className='italic pb-2'>{review.date}</p>
+            <img src={review.image} alt={`Image de ${review.title}`} className='mb-4 rounded w-screen mx-auto' />
+            <article dangerouslySetInnerHTML={{__html: review.body}} className='prose w-full max-w-none'/>
         </>
     )
 }
