7bb7c75d03af6d33ec433a664fdc871e9923c56f
berti <berti@CLASSES.LOCAL>
2025-01-07 12:04:03 +0100

    feature review sur home

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,24 @@
-'use client'
 import Heading from '@/components/Heading'
+import Link from 'next/link'
+import { getFeatureReview } from '@/lib/review'
 
-import { useEffect } from "react"
-
-export default function HomePage() {
-   
-    console.log('HomePage [Rending]')
+export default async function HomePage() {
+   const review = await getFeatureReview()
+    // console.log('HomePage [Rending]')
+    console.log(review)
     return (
         <>
             <Heading>My Games</Heading>
             <p>
                 Only the best games, reviewed for you.
             </p>
+            <div className="border rounded shadow hover:shadow-xl w-80 bg-slate-50">
+                <Link href={`/reviews/${review.slug}`}>
+                    <img src={review.image} alt={`image de ${review.title}`} className="rounded-t" />
+                    <h2 className="font-orbitron font-semibold py-1 text-center">{review.title}</h2>
+                </Link>
+            </div>
+
         </>
     )
 }
