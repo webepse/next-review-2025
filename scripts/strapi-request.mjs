a910a0322190e0bf6caf5eeecb1b19113da52595
berti <berti@CLASSES.LOCAL>
2025-01-14 09:37:22 +0100

    QS stringify

--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -1,5 +1,12 @@
 import { writeFileSync } from 'node:fs'
-const url = "http://localhost:1337/api/reviews" + "?populate=*" 
+import qs from 'qs'
+
+const url = "http://localhost:1337/api/reviews" + "?" + qs.stringify({
+    fields: ['slug','title','subtitle','publishedAt'],
+    populate: { image: {fields: ['url']}},
+    sort: ['publishedAt:desc'],
+    pagination: { pageSize: 6}
+}, {encodeValuesOnly: true}) 
 const response = await fetch(url)
 const body = await response.json()
 console.log(body)
