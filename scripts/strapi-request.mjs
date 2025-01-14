6c7ebd39c089040c80470ab86495bc0d952cc7b6
berti <berti@CLASSES.LOCAL>
2025-01-14 10:54:25 +0100

    getReviews

--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -2,10 +2,10 @@ import { writeFileSync } from 'node:fs'
 import qs from 'qs'
 
 const url = "http://localhost:1337/api/reviews" + "?" + qs.stringify({
-    fields: ['slug','title','subtitle','publishedAt'],
+    filters: {slug: { $eq: 'spiderman'}},
+    fields: ['slug','title','subtitle','publishedAt','body'],
     populate: { image: {fields: ['url']}},
-    sort: ['publishedAt:desc'],
-    pagination: { pageSize: 6}
+    pagination: { pageSize: 1, withCount: false}
 }, {encodeValuesOnly: true}) 
 const response = await fetch(url)
 const body = await response.json()
